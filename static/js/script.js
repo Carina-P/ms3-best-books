24436921aee617f9707cadcf46a36e866713e67f
Carina-P <carina.palmqvist@telia.com>
2020-11-30 16:33:15 +0000

    Added search in Google Books

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,43 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge:"right"});
-  });
-  
-$("#add_book").submit( () => {
-    text = "Innan fetch";
-    fetch("https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699")
-    .then(res =>res.json())
-    .then(res => {
-        console.log(res);
-        console.log(description);
-        text += res;
-    })
-    .catch(error => {
-        console.log("something is wrong", error);
-    });
-    console.log("efter fetch");
-    text += "Efter fetch";
-    $("#result").html("här är det" + text);
 });
+
+function searchTitle(){
+    console.log("Sök title");
+    let title = $("#search_txt").val();
+
+    if (!title || title.trim().length === 0){
+        console.log("ingen info");
+    }
+    else{
+        console.log(title);
+        fetch("https://www.googleapis.com/books/v1/volumes?q=intitle:"+ title+"&printType=books&projection=lite&key=AIzaSyAa48h04CAMjJ1bVewMoBx-_8EZv1IBNpI")
+        .then(res =>res.json())
+        .then(res => {
+            console.log(res);
+        })
+        .catch(error => {
+            console.log("something is wrong", error);
+        });
+    }
+}
+
+function searchAuthor(){
+    console.log("Sök författare");
+    let author = $("#search_txt").val();
+
+    if (!author || author.trim().length === 0){
+        console.log("ingen info");
+    }
+    else{
+        console.log(title);
+        fetch("https://www.googleapis.com/books/v1/volumes?q=intitle:"+ author+"&printType=books&projection=lite&key=AIzaSyAa48h04CAMjJ1bVewMoBx-_8EZv1IBNpI")
+        .then(res =>res.json())
+        .then(res => {
+            console.log(res);
+        })
+        .catch(error => {
+            console.log("something is wrong", error);
+        });
+    }
+}
