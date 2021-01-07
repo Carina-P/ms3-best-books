2b5fa63abea2e53a7c79f02fda13ffd8269ed133
Carina-P <carina.palmqvist@telia.com>
2021-01-07 17:49:37 +0000

    Added search-result-script only to that page

--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,6 @@
 let search_result = {};
 let category_groups = [];
 
-// This HTML "back-link" is copied from
-// https://stackoverflow.com/questions/8814472/how-to-make-an-html-back-link
-// by https://stackoverflow.com/users/1495198/vivek-maharajh
-let element = document.getElementById('back-link');
-element.setAttribute('href', document.referrer);
-/**
- * This function links user back to last page. And:
- * - Users get to hover over the link to see the URL
- * - Users don't end up with a corrupted back-stack
- */
-element.onclick = function() {
-  history.back();
-  return false;
-}
-
 /**
  * On smaller viewports menu should be removed when clicked
  */
