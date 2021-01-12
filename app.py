dec25b15c529a1dfee808ede27d20f8cb1127116
Carina-P <carina.palmqvist@telia.com>
2021-01-12 10:05:28 +0000

    Want to be able to debug in production environment but not by default

--- app.py
+++ app.py
@@ -825,4 +825,4 @@ def logout():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=bool(os.environ.get("IS_DEBUGGING")))
