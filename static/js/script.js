d3ce7c59b29b102118fd19d3d497d852eb84f450
Carina-P <carina.palmqvist@telia.com>
2021-01-14 06:31:39 +0000

    Bug in calling searchBooks

--- static/js/script.js
+++ static/js/script.js
@@ -220,6 +220,7 @@ function searchToDocument(){
                 if ("imageLinks" in book.volumeInfo){
                     if ("thumbnail" in book.volumeInfo.imageLinks){
                         let image_link = book.volumeInfo.imageLinks.thumbnail;
+                        
                         // Make sure the image_link is secure (https)
                         book.volumeInfo.imageLinks.thumbnail = image_link.replace("http:", "https:");
                         text += `<img src=${book.volumeInfo.imageLinks.thumbnail} alt="Picture of book cover">`;
